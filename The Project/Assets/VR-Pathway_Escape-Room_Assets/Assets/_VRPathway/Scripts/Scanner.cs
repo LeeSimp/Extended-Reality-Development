5322a80b0fc55c627b4a4e7545009fc5553672b5
LeeSimp <lsimpkin710@gmail.com>
2025-03-28 21:40:49 +0000

    A

--- The Project/Assets/VR-Pathway_Escape-Room_Assets/Assets/_VRPathway/Scripts/Scanner.cs
+++ The Project/Assets/VR-Pathway_Escape-Room_Assets/Assets/_VRPathway/Scripts/Scanner.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class Scanner : MonoBehaviour
+public class Scanner : XRGrabInteractable
 {
+    [Header("Scanner Data")]
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetBool("Opened", true);
     }
 
     // Update is called once per frame
