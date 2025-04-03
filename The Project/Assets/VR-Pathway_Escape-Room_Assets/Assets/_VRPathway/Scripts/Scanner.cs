866aaa746e75613768ca83458d13de093a50da46
LeeSimp <lsimpkin710@gmail.com>
2025-04-03 15:07:58 +0100

    broke the scanner

--- The Project/Assets/VR-Pathway_Escape-Room_Assets/Assets/_VRPathway/Scripts/Scanner.cs
+++ The Project/Assets/VR-Pathway_Escape-Room_Assets/Assets/_VRPathway/Scripts/Scanner.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class Scanner : XRGrabInteractable
 {
     [Header("Scanner Data")]
     public Animator animator;
+    public LineRenderer laserRenderer;
+    public TextMeshProUGUI targetName;
+    public TextMeshProUGUI targetPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +21,26 @@ public class Scanner : XRGrabInteractable
     void Update()
     {
         
+    }
+    protected override void Awake()
+    {
+        base.Awake();
+        ScannerActivated(false);
+    }
+    protected override void OnActivated(ActivateEventArgs args)
+    {
+        base.OnActivated(args);
+        ScannerActivated(true);
+    }
+    protected override void OnDeactivated(DeactivateEventArgs args)
+    {
+        base.OnDeactivated(args);
+        ScannerActivated(false);
+    }
+    private void ScannerActivated(bool isActivated)
+    {
+        laserRenderer.gameObject.SetActive(isActivated);
+        targetName.gameObject.SetActive(isActivated);
+        targetPosition.gameObject.SetActive(isActivated);
     }
 }
