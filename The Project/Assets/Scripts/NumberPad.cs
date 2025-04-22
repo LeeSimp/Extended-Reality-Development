68d537ecaf6157aa2390e249c6cb6a502a7d9b4c
Lee Simpkin <lsimpkin710@gmail.com>
2025-04-22 12:44:50 +0100

    Number pad is working

--- The Project/Assets/Scripts/NumberPad.cs
+++ The Project/Assets/Scripts/NumberPad.cs
@@ -2,71 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumberPad : MonoBehaviour
 {
     public string sequence;
-
-
     public TextMeshProUGUI inputDisplayText;
+    private string currentInput = "";
+    public DoorHandle doorHandle;
 
-    private string m_CurrentEnteredCode = "";
-
-    private void Awake()
+    public void ReceiveNumber(int number)
     {
-        inputDisplayText.text = "";
-    }
+        currentInput += number.ToString();
+        UpdateDisplay();
 
-    public void ButtonPressed(int valuePressed)
-    {
-        //append the number to the string (it will be converted to a string automatically but we could call the converting
-        //function explicitly to make it more clear : valuePressed.ToString()
-        m_CurrentEnteredCode += valuePressed;
-
-        //if it's the first character inputed, we reset the display string. if we were reseting when the code is entered
-        //we wouldn't have time to see the result (success/failure) so only reset on the next button press
-        if (m_CurrentEnteredCode.Length == 1)
+        if (currentInput.Length == sequence.Length)
         {
-            inputDisplayText.text = "*";
-            inputDisplayText.color = Color.black;
-        }
-        else //otherwise we append a new * to the string
-        {
-            inputDisplayText.text += "*";
-        }
-
-        if (m_CurrentEnteredCode.Length == sequence.Length)
-        {
-            //we finished typing the sequence, check if we have a number of right input equal to the sequence
-            if (m_CurrentEnteredCode == sequence)
-            {
-                Debug.Log("Right sequence entered");
-
-
-                inputDisplayText.text = "Code Valid!";
-                inputDisplayText.color = Color.green;
-            }
-            else
-            {
-                Debug.Log("Wrong sequenced entered");
-                inputDisplayText.text = "Invalid Code!";
-                inputDisplayText.color = Color.red;
-
-            }
-
-            //we reset the sequence checker
-            ResetSequence(false);
+            CheckCode();
         }
     }
 
-    public void ResetSequence(bool clearText)
+    private void UpdateDisplay()
     {
-        m_CurrentEnteredCode = "";
+        if (inputDisplayText != null)
+            inputDisplayText.text = currentInput;
+    }
 
-        if (clearText)
+    private void CheckCode()
+    {
+        if (currentInput == sequence)
         {
-            inputDisplayText.text = "";
-            inputDisplayText.color = Color.black;
+            UnlockDoor();
         }
+        else
+        {
+            ResetPad();
+        }
+    }
+
+    private void UnlockDoor()
+    {
+        inputDisplayText.text = "Unlocked!";
+        if (doorHandle != null)
+        {
+            doorHandle.Unlock(); // Call the method on DoorController
+        }
+    }
+
+    private void ResetPad()
+    {
+        currentInput = "";
+        UpdateDisplay();
     }
 }
--- The Project/Assets/Scripts/TouchButton.cs
+++ The Project/Assets/Scripts/TouchButton.cs
@@ -5,52 +5,38 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TouchButton : XRBaseInteractable
 {
-    [Header("Visuals")]
-    public Material normalMaterial;
-    public Material touchedMaterial;
+    public int buttonNumber; // Assign in Inspector
+    public Material hoverMaterial;
+    private Material originalMaterial;
+    private Renderer rend;
+    private NumberPad numberPad;
 
-    [Header("Button Data")]
-    public int buttonNumber;
-    public NumberPad linkedNumberpad;
-
-    private int m_NumberOfInteractor = 0;
-    private Renderer m_RendererToChange;
-
-    private void Start()
+    protected override void Awake()
     {
-        m_RendererToChange = GetComponent<MeshRenderer>();
+        base.Awake();
+        rend = GetComponent<Renderer>();
+        originalMaterial = rend.material;
+
+        // Find NumberPad in scene
+        numberPad = FindObjectOfType<NumberPad>();
     }
 
-    public override bool IsHoverableBy(IXRHoverInteractor interactor)
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        return base.IsHoverableBy(interactor) && (interactor is XRDirectInteractor);
-    }
+        base.OnSelectEntered(args);
 
-    protected override void OnHoverEntered(HoverEnterEventArgs args)
-    {
-        base.OnHoverEntered(args);
+        rend.material = hoverMaterial;
 
-        //we only want to change for the first interactor that touch (e.g. if we touch with a second hand when one is
-        //already touching the button, no need to change the data again)
-        if (m_NumberOfInteractor == 0)
+        if (numberPad != null)
         {
-            m_RendererToChange.material = touchedMaterial;
-
-            linkedNumberpad.ButtonPressed(buttonNumber);
+            numberPad.ReceiveNumber(buttonNumber);
         }
-
-        m_NumberOfInteractor += 1;
     }
 
-    protected override void OnHoverExited(HoverExitEventArgs args)
+    protected override void OnSelectExited(SelectExitEventArgs args)
     {
-        base.OnHoverExited(args);
+        base.OnSelectExited(args);
 
-        m_NumberOfInteractor -= 1;
-
-        //if we have multiple interactor touching that (e.g. 2 hands) then this will be called when only one is removed.
-        //we want to make sure we have nothing left hovering before returning it to "normal" state.
-        if (m_NumberOfInteractor == 0)
-            m_RendererToChange.material = normalMaterial;
+        rend.material = originalMaterial;
     }
 }
